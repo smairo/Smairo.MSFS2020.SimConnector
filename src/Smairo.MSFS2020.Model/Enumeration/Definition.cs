a2bb7c894ebb2ebc3220bf6ed50cb529dcf12996
Miro Holopainen <smairo@outlook.com>
2020-08-24 21:16:36 +0300

    feat: setup structs

--- src/Smairo.MSFS2020.Model/Enumeration/Definition.cs
+++ src/Smairo.MSFS2020.Model/Enumeration/Definition.cs
@@ -1,28 +1,30 @@
 ﻿namespace Smairo.MSFS2020.Model.Enumeration
 {
-    //public enum Definition
-    //{
-    //    All
-    //}
-
     public enum Definition
     {
-        PlaneAltitude,
-        FuelTotalQuantity,
-        UnlimitedFuelFlag,
-        PlaneLongitude,
-        PlaneLatitude,
-        AirSpeedTrue,
-        TotalWeight,
-        RealismPercentage,
-        RealismCrashDetection,
-        CrashFlag,
-        AtcType,
-        AtcModel,
-        AtcAirline,
-        AtcId,
-        AtcFlightNumber,
-        Title,
-        SimulationRate
+        PlaneMetadata,
+        PlaneVariable,
+        SimulationVariable
     }
+
+    //public enum Definition
+    //{
+    //    PlaneAltitude,
+    //    FuelTotalQuantity,
+    //    UnlimitedFuelFlag,
+    //    PlaneLongitude,
+    //    PlaneLatitude,
+    //    AirSpeedTrue,
+    //    TotalWeight,
+    //    RealismPercentage,
+    //    RealismCrashDetection,
+    //    CrashFlag,
+    //    AtcType,
+    //    AtcModel,
+    //    AtcAirline,
+    //    AtcId,
+    //    AtcFlightNumber,
+    //    Title,
+    //    SimulationRate
+    //}
 }
--- src/Smairo.MSFS2020.SimConnector/SimvarsViewModel.cs
+++ src/Smairo.MSFS2020.SimConnector/SimvarsViewModel.cs
@@ -9,18 +9,17 @@ using System.Windows.Threading;
 using Microsoft.FlightSimulator.SimConnect;
 using Smairo.MSFS2020.Model;
 using Smairo.MSFS2020.Model.Enumeration;
+using Smairo.MSFS2020.Model.Structs;
 using Smairo.MSFS2020.SimConnector.Interfaces;
 
 namespace Smairo.MSFS2020.SimConnector
 {
-    public class SimvarsViewModel : BaseViewModel, IBaseSimConnectWrapper
+    public class SimvarsViewModel : IBaseSimConnectWrapper
     {
         // User-defined win32 event
         public const int WmUserSimconnect = 0x0402;
         public bool Connected { get; private set; }
         private bool _collectionSet;
-
-        private readonly List<SimvarRequest> _requestedData = GetDataRequests();
 
         private IntPtr _hWnd = new IntPtr(0);
         private SimConnect _simConnection;
@@ -37,33 +36,44 @@ namespace Smairo.MSFS2020.SimConnector
             //if(_collectionSet) return;
             // TODO EX
 
-            //_simConnection?.RequestDataOnSimObjectType(
-            //   _reqId,
-            //   _defId,
-            //   0,
-            //   SIMCONNECT_SIMOBJECT_TYPE.USER);
+            _simConnection?.RequestDataOnSimObjectType(
+               Request.PlaneAltitude,
+               Definition.PlaneVariable,
+               0,
+               SIMCONNECT_SIMOBJECT_TYPE.USER);
 
-            var requests = _requestedData;
-            for (var i = 0; i < requests.Count; i++)
-            {
-                var simvarRequest = requests[i];
-                //_simConnection?.RequestDataOnSimObject(
-                //    simvarRequest.Request,
-                //    simvarRequest.Definition,
-                //    (uint) i,
-                //    SIMCONNECT_PERIOD.SECOND,
-                //    SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT,
-                //    0,
-                //    0,
-                //    0
-                //);
-                _simConnection?.RequestDataOnSimObjectType(
-                    simvarRequest.Request,
-                    simvarRequest.Definition,
-                   0,
-                   SIMCONNECT_SIMOBJECT_TYPE.USER);
-            }
+            _simConnection?.RequestDataOnSimObjectType(
+                Request.RealismCrashDetection,
+                Definition.SimulationVariable,
+                0,
+                SIMCONNECT_SIMOBJECT_TYPE.USER);
 
+            _simConnection?.RequestDataOnSimObjectType(
+                Request.Title,
+                Definition.PlaneMetadata,
+                0,
+                SIMCONNECT_SIMOBJECT_TYPE.USER);
+
+            //var requests = _requestedData;
+            //for (var i = 0; i < requests.Count; i++)
+            //{
+            //    var simvarRequest = requests[i];
+            //    //_simConnection?.RequestDataOnSimObject(
+            //    //    simvarRequest.Request,
+            //    //    simvarRequest.Definition,
+            //    //    (uint) i,
+            //    //    SIMCONNECT_PERIOD.SECOND,
+            //    //    SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT,
+            //    //    0,
+            //    //    0,
+            //    //    0
+            //    //);
+            //    _simConnection?.RequestDataOnSimObjectType(
+            //        simvarRequest.Request,
+            //        simvarRequest.Definition,
+            //       0,
+            //       SIMCONNECT_SIMOBJECT_TYPE.USER);
+            //}
             //foreach (var simvarRequest in _requestedData)
             //{
             //    _simConnection?.RequestDataOnSimObject(
@@ -104,18 +114,58 @@ namespace Smairo.MSFS2020.SimConnector
 
                 _simConnection.OnRecvClientData += OnRecvClientData;
 
-                foreach (var value in _requestedData)
+                var definition = Definition.PlaneMetadata;
+                foreach (var value in GetMetadataRequests())
                 {
                     _simConnection.AddToDataDefinition(
-                        value.Definition,
+                        definition,
                         value.NameUnitTuple.Name,
                         value.NameUnitTuple.Unit,
                         value.DataType,
                         0.0f,
                         SimConnect.SIMCONNECT_UNUSED);
-
-                    _simConnection.RegisterDataDefineStruct<double>(value.Definition);
                 }
+                _simConnection.RegisterDataDefineStruct<PlaneMetadatas>(definition);
+
+                definition = Definition.PlaneVariable;
+                foreach (var value in GetPlaneRequests())
+                {
+                    _simConnection.AddToDataDefinition(
+                        definition,
+                        value.NameUnitTuple.Name,
+                        value.NameUnitTuple.Unit,
+                        value.DataType,
+                        0.0f,
+                        SimConnect.SIMCONNECT_UNUSED);
+                }
+                _simConnection.RegisterDataDefineStruct<PlaneVariables>(definition);
+
+                definition = Definition.SimulationVariable;
+                foreach (var value in GetSimulationRequests())
+                {
+                    _simConnection.AddToDataDefinition(
+                        definition,
+                        value.NameUnitTuple.Name,
+                        value.NameUnitTuple.Unit,
+                        value.DataType,
+                        0.0f,
+                        SimConnect.SIMCONNECT_UNUSED);
+                }
+                _simConnection.RegisterDataDefineStruct<SimulationVariables>(definition);
+
+
+                //foreach (var value in _requestedData)
+                //{
+                //    _simConnection.AddToDataDefinition(
+                //        value.Definition,
+                //        value.NameUnitTuple.Name,
+                //        value.NameUnitTuple.Unit,
+                //        value.DataType,
+                //        0.0f,
+                //        SimConnect.SIMCONNECT_UNUSED);
+
+                //    _simConnection.RegisterDataDefineStruct<double>(value.Definition);
+                //}
             }
             catch (COMException ex)
             {
@@ -147,7 +197,25 @@ namespace Smairo.MSFS2020.SimConnector
             {
                 return;
             }
-            Console.WriteLine($"Val is: {data.dwRequestID} - {val}");
+
+            switch ((Definition) data.dwDefineID)
+            {
+                case Definition.PlaneMetadata:
+                    var metadata = (PlaneMetadatas) val;
+                    Console.WriteLine($"Plane is: {metadata.Title} - {metadata.AtcModel}");
+                    break;
+                case Definition.PlaneVariable:
+                    var planeVariables = (PlaneVariables) val;
+                    Console.WriteLine($"Plane is at: '{planeVariables.Latitude} {planeVariables.Longitude}' and flying in {planeVariables.Altitude} ft");
+                    break;
+                case Definition.SimulationVariable:
+                    var simulationVariables = (SimulationVariables) val;
+                    Console.WriteLine($"Can crash: {simulationVariables.RealismCrashDetection}. Crashing flag: {simulationVariables.CrashFlag}.");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            //Console.WriteLine($"Val is: {data.dwRequestID} - {val}");
         }
 
         private void OnRecvException(SimConnect sender, SIMCONNECT_RECV_EXCEPTION data)
@@ -185,114 +253,132 @@ namespace Smairo.MSFS2020.SimConnector
         {
         }
 
-        private static List<SimvarRequest> GetDataRequests()
+        private static List<SimvarRequest> GetSimulationRequests()
         {
             return new List<SimvarRequest>
             {
                 new SimvarRequest
                 {
-                    Definition = Definition.PlaneAltitude,
-                    NameUnitTuple = ( "PLANE ALTITUDE", "feet"),
-                    Request = Request.PlaneAltitude
-                },
-                new SimvarRequest
-                {
-                    Definition = Definition.PlaneLongitude,
-                    NameUnitTuple = ( "PLANE LONGITUDE", "degree"),
-                    Request = Request.PlaneLongitude
-                },
-                new SimvarRequest
-                {
-                    Definition = Definition.PlaneLatitude,
-                    NameUnitTuple = ( "PLANE LATITUDE", "degree"),
-                    Request = Request.PlaneLatitude
-                },
-                new SimvarRequest
-                {
-                    Definition = Definition.FuelTotalQuantity,
-                    NameUnitTuple = ( "FUEL TOTAL QUANTITY", "gallons"),
-                    Request = Request.FuelTotalQuantity
-                },
-                new SimvarRequest
-                {
-                    Definition = Definition.AirSpeedTrue,
-                    NameUnitTuple = ( "AIRSPEED TRUE", "knots"),
-                    Request = Request.AirSpeedTrue
-                },
-                new SimvarRequest
-                {
-                    Definition = Definition.TotalWeight,
-                    NameUnitTuple = ( "TOTAL WEIGHT", "pounds"),
-                    Request = Request.TotalWeight
-                },
-                new SimvarRequest
-                {
-                    Definition = Definition.UnlimitedFuelFlag,
+                    Definition = Definition.SimulationVariable,
                     NameUnitTuple = ( "UNLIMITED FUEL", "bool"),
                     Request = Request.UnlimitedFuelFlag
                 },
                 new SimvarRequest
                 {
-                    Definition = Definition.RealismPercentage,
+                    Definition = Definition.SimulationVariable,
                     NameUnitTuple = ( "REALISM", "number"),
                     Request = Request.RealismPercentage
                 },
                 new SimvarRequest
                 {
-                    Definition = Definition.RealismCrashDetection,
+                    Definition = Definition.SimulationVariable,
                     NameUnitTuple = ( "REALISM CRASH DETECTION", "bool"),
                     Request = Request.RealismCrashDetection
                 },
                 new SimvarRequest
                 {
-                    Definition = Definition.SimulationRate,
+                    Definition = Definition.SimulationVariable,
                     NameUnitTuple = ( "SIMULATION RATE", "number"),
                     Request = Request.SimulationRate
                 },
                 new SimvarRequest
                 {
-                    Definition = Definition.CrashFlag,
+                    Definition = Definition.SimulationVariable,
                     NameUnitTuple = ( "CRASH FLAG", "enum"),
                     Request = Request.CrashFlag
-                },
+                }
+            };
+        }
 
-                //new SimvarRequest
-                //{
-                //    Definition = Definition.AtcType,
-                //    NameUnitTuple = ( "ATC TYPE", ""),
-                //    Request = Request.AtcType,
-                //    DataType = SIMCONNECT_DATATYPE.STRING256
-                //},
-                //new SimvarRequest
-                //{
-                //    Definition = Definition.AtcModel,
-                //    NameUnitTuple = ( "ATC MODEL", ""),
-                //    Request = Request.AtcModel
-                //},
-                //new SimvarRequest
-                //{
-                //    Definition = Definition.AtcAirline,
-                //    NameUnitTuple = ( "ATC AIRLINE", ""),
-                //    Request = Request.AtcAirline
-                //},
-                //new SimvarRequest
-                //{
-                //    Definition = Definition.AtcId,
-                //    NameUnitTuple = ( "ATC ID", ""),
-                //    Request = Request.AtcId
-                //},
-                //new SimvarRequest
-                //{
-                //    Definition = Definition.AtcFlightNumber,
-                //    NameUnitTuple = ( "ATC FLIGHT NUMBER", ""),
-                //    Request = Request.AtcFlightNumber
-                //},
-                //new SimvarRequest
-                //{
-                //    Definition = Definition.Title,
-                //    NameUnitTuple = ( "TITLE", ""),
-                //    Request = Request.Title
-                //}
+        private static List<SimvarRequest> GetPlaneRequests()
+        {
+            return new List<SimvarRequest>
+            {
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneVariable,
+                    NameUnitTuple = ( "PLANE ALTITUDE", "feet"),
+                    Request = Request.PlaneAltitude
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneVariable,
+                    NameUnitTuple = ( "PLANE LONGITUDE", "degree"),
+                    Request = Request.PlaneLongitude
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneVariable,
+                    NameUnitTuple = ( "PLANE LATITUDE", "degree"),
+                    Request = Request.PlaneLatitude
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneVariable,
+                    NameUnitTuple = ( "FUEL TOTAL QUANTITY", "gallons"),
+                    Request = Request.FuelTotalQuantity
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneVariable,
+                    NameUnitTuple = ( "AIRSPEED TRUE", "knots"),
+                    Request = Request.AirSpeedTrue
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneVariable,
+                    NameUnitTuple = ( "TOTAL WEIGHT", "pounds"),
+                    Request = Request.TotalWeight
+                },
+            };
+        }
+
+        private static List<SimvarRequest> GetMetadataRequests()
+        {
+            return new List<SimvarRequest>
+            {
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneMetadata,
+                    NameUnitTuple = ( "ATC TYPE", ""),
+                    Request = Request.AtcType,
+                    DataType = SIMCONNECT_DATATYPE.STRING256
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneMetadata,
+                    NameUnitTuple = ( "ATC MODEL", ""),
+                    Request = Request.AtcModel,
+                    DataType = SIMCONNECT_DATATYPE.STRING256
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneMetadata,
+                    NameUnitTuple = ( "ATC AIRLINE", ""),
+                    Request = Request.AtcAirline,
+                    DataType = SIMCONNECT_DATATYPE.STRING256
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneMetadata,
+                    NameUnitTuple = ( "ATC ID", ""),
+                    Request = Request.AtcId,
+                    DataType = SIMCONNECT_DATATYPE.STRING256
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneMetadata,
+                    NameUnitTuple = ( "ATC FLIGHT NUMBER", ""),
+                    Request = Request.AtcFlightNumber,
+                    DataType = SIMCONNECT_DATATYPE.STRING256
+                },
+                new SimvarRequest
+                {
+                    Definition = Definition.PlaneMetadata,
+                    NameUnitTuple = ( "TITLE", ""),
+                    Request = Request.Title,
+                    DataType = SIMCONNECT_DATATYPE.STRING256
+                }
             };
         }
     }
